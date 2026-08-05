785b2b368652254352806c3f70c9374e5395043d
juzihaochi666 <1538808269@qq.com>
2026-08-05 15:13:57 +0800

    实现了react改造

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  plugins: [react()],
+  
   server: {
     host: '127.0.0.1',  // 强制使用 IPv4，避免 localhost 解析到 ::1
     port: 5173,          // 默认端口
     open: true,          // 自动在浏览器打开
   },
 });
+
