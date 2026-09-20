2d6aef6b22e619ea842c0a1e70c2fb9fa034261c
juzihaochi666 <1538808269@qq.com>
2026-09-20 15:03:31 +0800

    后端CORS名单抽取为配置文件，补充.env.example

--- backend/main.py
+++ backend/main.py
@@ -6,13 +6,19 @@ from pypinyin import lazy_pinyin, Style
 from snownlp import SnowNLP
 from storage import init_db,save_record, get_history
 from datetime import datetime, timezone
+import os
+from dotenv import load_dotenv
+
+load_dotenv()                        # ← 读同目录下的 .env
+
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS").split(",")
 
 init_db()  # 初始化数据库
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=ALLOWED_ORIGINS,
     allow_methods=["GET", "POST"],
     allow_credentials=True,
 )
